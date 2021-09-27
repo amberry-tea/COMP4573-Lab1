3b9d5ae29fb05957a43b1c22b82dc55c0c7444a4
Andrew Martin <andrew.martin455@gmail.com>
2021-09-26 19:30:44 -0700

    Implement writer.js local storage usage

--- js/writer.js
+++ js/writer.js
@@ -1,7 +1,7 @@
 var notes = [];
 
 function removeNote(source){
-    let index = parseInt(source.parentNode.id.substring(4));
+    index = parseInt(source.parentNode.id.substring(4));
     
     // Remove this note from the ntoes
     console.log("Removing note number " + index + " from :\n" + notes);
@@ -12,7 +12,7 @@ function removeNote(source){
     source.parentNode.remove();
 
     // Rename the other notes IDs to replace them with an index that is one lower (to preserve order)
-    for(var i = index; i < notes.length + 1; i++){
+    for(var i = index + 1; i < notes.length + 1; i++){
         document.getElementById("note"+i).id = "note"+(i-1);
     }
 }
@@ -23,22 +23,49 @@ function addNote(){
 
     // Add a new element to the document for the note
     notesDiv = document.getElementById("notes");
-    notesDiv.innerHTML += 
-        `
-        <div id="note` + (notes.length - 1) + `">
-            <div class="noteOuter">
-                <textarea class="noteInner">
-                </textarea>
-            </div>
-            <button class="remove" onclick="removeNote(this);">Remove</button>
-            </br>
-        </div>
-        `;
+
+    note = document.createElement("div");
+    note.setAttribute("id", "note"+(notes.length-1));
+
+    noteOuter = document.createElement("div");
+    noteOuter.setAttribute("class", "noteOuter");
+    note.appendChild(noteOuter);
+
+    noteInner = document.createElement("textArea");
+    noteInner.setAttribute("class", "noteInner");
+    noteOuter.appendChild(noteInner);
+
+    button = document.createElement("button");
+    button.setAttribute("class", "remove");
+    button.setAttribute("onclick", "removeNote(this);");
+    button.innerHTML = "Remove";
+    note.appendChild(button);
+
+    notesDiv.appendChild(note);
+
+
+    // notesDiv.innerHTML += 
+    //     `
+    //     <div id="note` + (notes.length-1) + `">
+    //         <div class="noteOuter">
+    //             <textarea class="noteInner">
+    //             </textarea>
+    //         </div>
+    //         <button class="remove" onclick="removeNote(this);">Remove</button>
+    //         </br>
+    //     </div>
+    //     `;
 }
 
 function updateLocalStorage(){
+    console.log("Update!");
+    for(i = 0; document.getElementById("note" + i) != null; i++){
+        notes[i] = document.getElementById("note" + i).children[0].children[0].value;
+    }
+
+    console.log(notes);
     localStorage.setItem("notes", JSON.stringify(notes));
 }
 
 // Run the update of local cache every two seconds
-window.setInterval(updateLocalStorage(), 2000);
+window.setInterval(updateLocalStorage, 2000);
