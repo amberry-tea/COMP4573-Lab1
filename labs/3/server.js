f4708f49d1115d164ca1a2c7e1d2989bd9aa75f1
Tomas Haley-Ohrt <t.haleyohrt@gmail.com>
2021-10-09 19:32:42 -0700

    Changed message

--- labs/3/server.js
+++ labs/3/server.js
@@ -6,7 +6,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-type': 'text/html'});
     const queryStr = url.parse(req.url, true);
     const qdata = queryStr.query;
-    const msg = `Hi, ${qdata.name}! Server current date is ${d.getDate()}`;
+    const msg = `Hi, ${qdata.name}! What a beautiful day. Server current date and time is ${d.getDate()}`;
     res.write(`<p style="color:blue;"><strong>${msg}</strong></p>`);
     res.end();
 }).listen(8888);
