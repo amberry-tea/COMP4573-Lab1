b3a6331fa5c7d054fb75f26f7852f58139b91c24
Andrew Martin <andrew.martin455@gmail.com>
2021-10-17 22:42:49 -0700

    Implement frontend client code

--- labs/4/frontend/js/search.js
+++ labs/4/frontend/js/search.js
@@ -1,8 +1,11 @@
 const invalidInputMessage = "Invalid input! Ensure input is only letters and spaces.";
-const serverQueryUrl = "http://unknownmorph.com/COMP4537/labs/4/api/definitions?word="
+const serverQueryUrl = "http://unknownmorph.com/COMP4537/labs/4/api/definitions?word=";
+const termText = "Term: ";
+const definitionText = "</br>Definition: ";
+const requestsText = "</br>Requests: ";
 
 function search(source){
-    searchTerm = document.getElementById("input-textarea").value;
+    searchTerm = document.getElementById("input-word").value;
     resultTextElement = document.getElementById("inner-results");
     
     if(/[^a-zA-Z ]/.test(searchTerm)){ // Ensure input is only letters and spaces
@@ -21,10 +24,19 @@ function search(source){
         xhttp.send();
         xhttp.onreadystatechange = function(){
             if(this.readyState == 4){ //Validate that the response is okay
-                //resultTextElement.value = 
-                console.log(this);
+                console.log(xhttp.responseText);
+                json = JSON.parse(xhttp.responseText)
+                definition = json.definition;
+                term = json.term;
+                requests = json.requests;
+                if(xhttp.status == 200){
+                    resultTextElement.innerHTML = termText + term + definitionText + definition + requestsText + requests;
+                } else {
+                    responseText = JSON.parse(xhttp.responseText).message;
+                    resultTextElement.innerHTML = responseText + requestsText + requests;
+                }
             } else {
-                console.log("readyState = " + this.readyState + "\nstate = " + this.state)
+                console.log("readyState = " + this.readyState + "\nstate = " + this.state);
             }
         };
 
--- labs/4/frontend/js/store.js
+++ labs/4/frontend/js/store.js
@@ -1,39 +1,52 @@
 const serverQueryUrl = "http://unknownmorph.com/COMP4537/labs/4/api/definitions/";
 const sendingMessageText = "Sending new definiton to the server...";
-const recievedMessageText = "New definition added!";
+const successMessageText = "New definition added!";
+// const preExistingDefinitionMessageText = "Definition is already in the dictionary!";
+const badInputMessageText = "Word must only contain characters within A-Z, a-z and spaces!";
+const emptyInputMessageText = "Enter a word and definition!";
 
 function store(source) {
-    searchTerm = document.getElementById("input-textarea").value;
+    newWord = document.getElementById("input-word").value;
+    newDefinition = document.getElementById("input-definition").value;
+
     resultTextElement = document.getElementById("inner-results");
 
-    if (/[^a-zA-Z ]/.test(searchTerm)) { // Ensure input is only letters and spaces
+    if (!newWord || !newDefinition){
         console.log("Bad input!");
 
-        resultTextElement.value = invalidInputMessage;
-    } else {
-        console.log("Good input!");
-        resultTextElement.value = sendingMessageText;
+        resultTextElement.innerHTML = emptyInputMessageText;
+    } else if (/[^a-zA-Z ]/.test(newWord)) { // Ensure input is only letters and spaces
+        console.log("Bad input!");
 
-        searchTerm = searchTerm.trim();
+        resultTextElement.innerHTML = badInputMessageText;
+    } else  {
+        console.log("Good input!");
+        resultTextElement.innerHTML = sendingMessageText;
+
+        newWord = newWord.trim();
 
         url = serverQueryUrl;
         params = {
-            'word': 'Book',
-            'definition': 'A written or printed work consisting of pages glued or sewn together'
+            'word': newWord,
+            'definition': newDefinition
         };
-        //params = "?word=book&definition=A written or printed work consisting of pages glued or sewn together"
 
         const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4) { //Validate the resonse is okay
                 console.log(xhttp.responseText);
-                //resultTextElement.value = recievedMessageText;
+                responseText = JSON.parse(xhttp.responseText).message;
+                if(xhttp.status == 200){
+                    resultTextElement.innerHTML = successMessageText;
+                } else {
+                    resultTextElement.innerHTML = responseText;
+                }
             } else {
-                console.log("readyState = " + this.readyState + "\nstate = " + this.state)
+                console.log("readyState = " + this.readyState + "\nstate = " + this.state);
             }
         };
         xhttp.open("POST", url, true);
-        xhttp.setRequestHeader('Content-Type', 'application/json');
+        xhttp.setRequestHeader('Content-Type', 'text/plain');
         xhttp.send(JSON.stringify(params)); //params in the body
     }
 }
